8621b920b6229e82c4047d0348e011eb57dd40d9
rb <rafabuenop@uol.com.br>
2023-11-17 15:29:04 -0300

    v3

--- pokefunc.py
+++ pokefunc.py
@@ -1,7 +1,4 @@
-from collections.abc import KeysView
-
 pokedex = {}
-
 
 def AddPoke():
   escolha = 'SIM'
@@ -9,7 +6,7 @@ def AddPoke():
     identificacao = len(pokedex) + 1
     pokemon_Nome = str(input('Digite o nome do Pokemon: ')).upper()
     tipo_Pokemon = str(input('Digite o tipo do Pokemon: ')).upper()
-    nivel_Pokemon = int(input('Digite o nível do Pokemon: '))
+    nivel_Pokemon = float(input('Digite o nível do Pokemon: '))
 
     pokemon = {
         'id': identificacao,
@@ -27,15 +24,27 @@ def listar_pokemon():
   # Itera sobre os Pokémon na Pokedex
   for identificacao, pokemon in pokedex.items():
     # Exibe as informações do Pokémon
-    print(
-        f'|ID: {identificacao:3}| Nome: {pokemon["nome"]:10}|Tipo: {pokemon["tipo"]:10} | Nível: {pokemon["nivel"]:3} |'
-    )
-
+    print(f'|ID: {identificacao:3}| Nome: {pokemon["nome"]:10}|Tipo: {pokemon["tipo"]:10} | Nível: {pokemon["nivel"]:3} |')
 
 def excluir_pokemon(id):
-  id_excluir = id - 1
   del pokedex[id]
+  print('Pokemón deletado com sucesso')
 
+def atualizarPokemon(id, nome, tipo, nivel):
+  pokemon = pokedex[id]
+  pokemon['nome'] = nome
+  pokemon['tipo'] = tipo
+  pokemon['nivel'] = nivel
 
-def pesquisaPokemon(id):
-  print(pokedex[id - 1])
+def pesquisaPokemonNome(nome):
+  for identificacao, pokemon in pokedex.items():
+    if pokemon['nome'] == nome:
+      print(f'| ID: {pokemon["id"]} | Nome: {pokemon["nome"]} | Tipo: {pokemon["tipo"]} | Nivel: {pokemon["nivel"]} |')
+
+def pesquisaPokemonTipo(tipo):
+  for identificacao, pokemon in pokedex.items():
+    if pokemon['tipo'] == tipo:
+      print(f'| ID: {pokemon["id"]} | Nome: {pokemon["nome"]} | Tipo: {pokemon["tipo"]} | Nivel: {pokemon["nivel"]} |')
+
+def contarPokemon():
+  print(f'A quantidade de Pokemóns registrados são de {len(pokedex)} Pokemóns')
