043c5335c920c3b5c60c4368d2876f4ea5e8c55a
rb <rafabuenop@uol.com.br>
2023-11-16 22:19:21 -0300

    v2-2

--- pokefunc.py
+++ pokefunc.py
@@ -1,4 +1,8 @@
-pokedex = []
+from collections.abc import KeysView
+
+pokedex = {}
+
+
 def AddPoke():
   escolha = 'SIM'
   while escolha == 'SIM':
@@ -6,24 +10,32 @@ def AddPoke():
     pokemon_Nome = str(input('Digite o nome do Pokemon: ')).upper()
     tipo_Pokemon = str(input('Digite o tipo do Pokemon: ')).upper()
     nivel_Pokemon = int(input('Digite o nível do Pokemon: '))
+
     pokemon = {
-      'id': identificacao, 
-      'nome': pokemon_Nome, 
-      'tipo': tipo_Pokemon, 
-      'nivel': nivel_Pokemon}
-    pokedex.append(pokemon)
+        'id': identificacao,
+        'nome': pokemon_Nome,
+        'tipo': tipo_Pokemon,
+        'nivel': nivel_Pokemon
+    }
+
+    pokedex[identificacao] = pokemon
     escolha_input = input('Deseja adicionar outro Pokemon?\nSIM/NÃO: ').upper()
     escolha = escolha_input
   return pokedex
 
 def listar_pokemon():
-    for c in range(0, len(pokedex)):
-         print(pokedex[c])
-    print(f'A quantidade de Pokemóns cadastrados é de: {len(pokedex)}')
+  # Itera sobre os Pokémon na Pokedex
+  for identificacao, pokemon in pokedex.items():
+    # Exibe as informações do Pokémon
+    print(
+        f'|ID: {identificacao:3}| Nome: {pokemon["nome"]:10}|Tipo: {pokemon["tipo"]:10} | Nível: {pokemon["nivel"]:3} |'
+    )
+
 
 def excluir_pokemon(id):
-   id_excluir = id - 1
-   pokedex.pop(id_excluir)
+  id_excluir = id - 1
+  del pokedex[id]
+
 
 def pesquisaPokemon(id):
-   print(pokedex[id-1])
+  print(pokedex[id - 1])
